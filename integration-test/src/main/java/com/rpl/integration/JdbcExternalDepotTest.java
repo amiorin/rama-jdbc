50354ed363733d7fcd641b307c8954468a9a9edc
Alberto Miorin <109069886+alberto-of@users.noreply.github.com>
2025-07-01 12:48:27 +0200

    wip: try to run it now

--- integration-test/src/main/java/com/rpl/integration/JdbcExternalDepotTest.java
+++ integration-test/src/main/java/com/rpl/integration/JdbcExternalDepotTest.java
@@ -1,21 +1,22 @@
 package com.rpl.integration;
 
 import com.rpl.rama.*;
+import com.rpl.rama.jdbc.JdbcExternalDepot;
 import com.rpl.rama.module.*;
 import com.rpl.rama.ops.Ops;
 import com.rpl.rama.test.*;
-import org.apache.kafka.clients.consumer.*;
-import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.serialization.*;
-
-import java.time.Duration;
-import java.util.*;
 
 public class JdbcExternalDepotTest {
   public static class JdbcExternalDepotModule implements RamaModule {
     @Override
     public void define(Setup setup, Topologies topologies) {
-        System.out.println("Done!");
+      setup.declareObject("*jdbc", new JdbcExternalDepot());
+
+      StreamTopology s = topologies.stream("s");
+      s.source("*jdbc", StreamSourceOptions.startFromBeginning()).out("*long")
+       .each(Ops.PRINTLN, "*long")
+       .execute();
+      System.out.println("Done!");
     }
   }
 
@@ -26,6 +27,7 @@ public class JdbcExternalDepotTest {
         String moduleName = module.getClass().getName();
         System.out.println("Launching module...");
         cluster.launchModule(module, new LaunchConfig(4, 2));
+        Thread.sleep(10 * 1000);
         System.out.println("Done!");
       }
   }
