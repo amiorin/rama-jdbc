084d2e7603033a9d74b276951bbe5dea9fbb2d5e
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2025-10-19 18:45:22 +0200

    fix: commit the static files

---
+++ devenv.nix
@@ -0,0 +1,14 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = [
+    pkgs.git
+    pkgs.process-compose
+    pkgs.babashka
+    pkgs.python3
+    pkgs.postgresql_16
+    pkgs.just
+    pkgs.sql-migrate
+  ];
+  languages.clojure.enable = true;
+}
